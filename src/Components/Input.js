2b25657818bcf763ec909b37b31a3073ce5eed46
martiN <78382341+martynosa@users.noreply.github.com>
2022-12-12 23:35:05 +0200

    css cleanup

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -10,7 +10,7 @@ export default function Input({ setLocation }) {
   };
 
   return (
-    <div className="group">
+    <div className="input-group">
       <input
         ref={inputEl}
         type="text"
