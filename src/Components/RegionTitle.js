9133e099317f4ea72fad1e546881333cf753d351
martiN <78382341+martynosa@users.noreply.github.com>
2022-12-12 22:28:21 +0200

    loading for the title

--- src/Components/RegionTitle.js
+++ src/Components/RegionTitle.js
@@ -4,6 +4,6 @@ export default function RegionTitle({ location, loading }) {
   const regionTitle = (
     <h1 className="region-title">{`${location?.area} ${location?.region} ${location?.country}`}</h1>
   );
-  const loadingTitle = <h1 className="region-title">&nbsp;</h1>;
+  const loadingTitle = <h1 className="region-title loading-title">&nbsp;</h1>;
   return loading ? loadingTitle : regionTitle;
 }
